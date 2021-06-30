16ef4140308c2258e9355ecb568bd720c22679f1
zat-is-me <taahm1@morgan.edu>
2021-06-29 20:48:05 -0500

    initial commit

---
+++ src/app/common/product.ts
@@ -0,0 +1,13 @@
+export class Product {
+    sku!: String;
+    name!: String;
+    description!: String;
+    unitPrice!: String;
+    imageUrl!: String;
+    active!: boolean;
+    unitsInStock!: number;
+    dateCreated!: Date;
+    lastUpdated!: Date;
+
+}
+
